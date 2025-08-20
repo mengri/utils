87ff513ec257416cfdfd763fa090bf80a31c25ca
黄孟柱 <7427565+huang_meng_zhu@user.noreply.gitee.com>
2025-08-20 11:31:51 +0800

    增加通用registers

---
+++ register/registers.go
@@ -0,0 +1,40 @@
+package register
+
+import (
+	"fmt"
+	"github.com/mengri/utils/untyped"
+)
+
+type Registers[T any] interface {
+	Register(name string, T any) error
+	ALl() []T
+	Get(name string) (T, bool)
+}
+
+type imlRegisters[T any] struct {
+	objects untyped.Untyped[string, T]
+}
+
+func newImlRegisters[T any]() Registers[T] {
+
+	return &imlRegisters[T]{
+		objects: untyped.BuildUntyped[string, T](),
+	}
+}
+
+func (rs *imlRegisters[T]) Register(name string, T any) error {
+	_, has := rs.objects.Get(name)
+	if has {
+		return fmt.Errorf("%s exits", name)
+	}
+	rs.objects.Set(name, T)
+	return nil
+}
+
+func (rs *imlRegisters[T]) ALl() []T {
+	return rs.objects.List()
+}
+
+func (rs *imlRegisters[T]) Get(name string) (T, bool) {
+	return rs.objects.Get(name)
+}
